b884236268d46ce2115408285d51b5387856af99
Rajarshi Ghosh <rajarshighosh6228@gmail.com>
2026-06-17 08:32:31 +0530

    Csv loaded to python

--- src/ingestion/data_loader.py
+++ src/ingestion/data_loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-customer_df = pd.read_csv("data/raw/Add Spend Dataset.csv")
+Addspend_df = pd.read_csv("data/raw/Add Spend Dataset.csv")
 interaction_df = pd.read_csv("data/raw/Customer Interaction Dataset.csv")
 revenue_df = pd.read_csv("data/raw/Revenue Dataset.csv")
 
-print(customer_df.head())
+print(Addspend_df.head())
 print(interaction_df.head())
 print(revenue_df.head())
