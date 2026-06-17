1a67b97b1719d884599eca5277ddfe55024ec25b
Rajarshi Ghosh <rajarshighosh6228@gmail.com>
2026-06-17 10:58:18 +0530

    Builds the customer journey.

---
+++ src/attribution/attribution_engine.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+
+def build_customer_journeys():
+
+    interaction_df = pd.read_csv(
+        "data/processed/cleaned Customer Interaction Dataset.csv"
+    )
+
+    revenue_df = pd.read_csv(
+        "data/processed/cleaned Revenue Dataset.csv"
+    )
+
+    interaction_df["Interaction_date"] = pd.to_datetime(
+        interaction_df["Interaction_date"],
+        dayfirst=True,
+        errors="coerce"
+    )
+
+    interaction_df = interaction_df.sort_values(
+        by=["User_id", "Interaction_date"]
+    )
+
+    journey_df = (
+        interaction_df.groupby("User_id")
+        .agg({
+            "Channel": lambda x: " > ".join(x),
+            "Campaign_id": lambda x: " > ".join(x)
+        })
+        .reset_index()
+    )
+
+    journey_df.rename(
+        columns={"Channel": "Journey"},
+        inplace=True
+    )
+
+    journey_df = journey_df.merge(
+        revenue_df,
+        on="User_id",
+        how="left"
+    )
+
+    journey_df["Journey_Length"] = (
+        journey_df["Journey"]
+        .str.split(" > ")
+        .str.len()
+    )
+
+    journey_df.to_csv(
+        "data/processed/customer_journeys.csv",
+        index=False
+    )
+
+    print("Customer journeys created successfully")
+
+
+if __name__ == "__main__":
+    build_customer_journeys()
