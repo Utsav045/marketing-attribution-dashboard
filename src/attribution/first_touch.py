4ddd58dfe03d367d94673c67ccf916ba703526c0
Rajarshi Ghosh <rajarshighosh6228@gmail.com>
2026-06-17 11:11:25 +0530

    First Touch code implemented

---
+++ src/attribution/first_touch.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+def first_touch_attribution():
+
+    df = pd.read_csv(
+        "data/processed/customer_journeys.csv"
+    )
+
+    df["First_Touch"] = (
+        df["Journey"]
+        .str.split(" > ")
+        .str[0]
+    )
+
+    result = (
+        df.groupby("First_Touch")["Revenue"]
+        .sum()
+        .reset_index()
+    )
+
+    result.rename(
+        columns={"Revenue": "Attributed_Revenue"},
+        inplace=True
+    )
+
+    result.to_csv(
+        "data/processed/first touch results.csv",
+        index=False
+    )
+
+    print(result)
+
+
+if __name__ == "__main__":
+    first_touch_attribution()
